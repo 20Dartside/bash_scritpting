c8bc5ff1fdf29f8e88de8a9f332df551fe354655
root <root@ip-172-31-32-60.eu-west-3.compute.internal>
2025-06-22 06:08:12 +0000

    extracting ip_address

---
+++ ip_address.sh
@@ -0,0 +1,12 @@
+# extracting ip_address 
+
+#! /bin/bash
+read -p 'enter username ' u
+echo "Your name is $u" 
+cat ip_output | grep inet
+
+ip_output=$(ifconfig &)
+ip_address=$(echo "$ip_output" | grep 'inet ' | awk '{print $2}' | head -n 1)
+
+echo "Your IP address is: $ip_address"
+
